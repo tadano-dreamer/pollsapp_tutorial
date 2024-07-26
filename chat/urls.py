520315b75551dc72d939b67ff4a4536d0019547c
YusukeTomy <132047095+tadano-dreamer@users.noreply.github.com>
2024-07-26 13:46:19 +0900

    Delete urls.py

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, reverse_lazy
-from django.contrib.auth import views as auth_views
-
-from . import views
-
-app_name = "chat"
-urlpatterns = [
-    path("", views.index, name="index"),
-]
